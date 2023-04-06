4c164e1c57a00008a76c273c64ca840436705c30
Boris Pavel <boris.pavel@gmx.com>
2023-04-06 22:35:45 +0300

    Add layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-console.log("hello!!");
+import "./normalize.css"
+import "./style.css";
+
+const createHeader = (...headerElementsText) => {
+  const header = document.createElement("div");
+  header.classList.add("header");
+  const ul = document.createElement("ul");
+
+  headerElementsText.forEach((headerElement) => {
+    const li = document.createElement("li");
+    li.textContent = headerElement;
+    ul.appendChild(li);
+  });
+  header.appendChild(ul);
+
+  return header;
+};
+
+const createSection = () => {
+  const section = document.createElement("div");
+  section.classList.add("section");
+
+  return section;
+};
+
+const createLinks = (...sentences) => {
+    const container = [];
+    sentences.forEach((sentence) => {
+      const a = document.createElement("a");
+      a.href = "#";
+      a.textContent = sentence;
+      container.push(a);
+    });
+    return container;
+  };
+
+const createFooter = (footerElements) => {
+  const footer = document.createElement("div");
+  footer.classList.add("footer");
+  const ul = document.createElement("ul");
+
+  footerElements.forEach((footerElement) => {
+    const li = document.createElement("li");
+    li.append(footerElement);
+    ul.appendChild(li);
+  });
+  footer.appendChild(ul);
+
+  return footer;
+};
+
+
+
+const displayLayout = ((header, section, footer) => {
+  const content = document.querySelector("#content");
+
+  content.append(header);
+  content.append(section);
+  content.append(footer);
+})(createHeader("Home", "Menu", "Contact"), createSection(), createFooter(createLinks("sadfasf sadf asdf asd","lol")));
